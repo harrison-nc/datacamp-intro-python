65aacb4b34054eff558aa95b795a3f64e86a7708
Harrison Chikwe <nhcdeveloper@gmail.com>
2023-01-29 20:57:06 +0100

    Type conversion: convert string to float

---
+++ scripts/type_convert.py
@@ -0,0 +1,20 @@
+# Instruction
+# -----------
+# 1. Convert the variable pi_string to a float
+#    and store this float as a new variable,
+#    pi_float.
+# 2. Print the variable pi_float.
+
+
+
+# Solution
+# --------
+
+# Definition of pi_string
+pi_string = "3.1415926"
+
+# Convert pi_string ino float: pi_float
+pi_float = float(pi_string)
+
+# Print out pi_float
+print(pi_float)
