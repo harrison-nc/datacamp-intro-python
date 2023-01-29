0eec95f560c5a11a0b19c795ca0c60c414c71d89
Harrison Chikwe <nhcdeveloper@gmail.com>
2023-01-29 21:05:01 +0100

    Use data type: list

---
+++ scripts/house/area.py
@@ -0,0 +1,26 @@
+# Instruction
+# -----------
+# 1. Create a list, areas, that contains the
+#    area of the hallway (hall), kitchen (kit),
+#    living room (liv), bedroom (bed) and
+#    bathroom (bath), in this order. Use the
+#    predefined variables.
+# 2. Print areas with the print() function.
+
+
+
+# Solution
+# --------
+
+# area variables (in square meters)
+hall = 11.25
+kit = 18.0
+liv = 20.0
+bed = 10.75
+bath = 9.58
+
+# Create list areas
+areas = [hall, kit, liv, bed, bath]
+
+# Print areas
+print(areas)
