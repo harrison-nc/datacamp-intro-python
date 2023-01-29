25344123e73399f1f2e1f8819829955bccce7bca
Harrison Chikwe <nhcdeveloper@gmail.com>
2023-01-29 22:00:35 +0100

    Use data type: list; add list elements together

---
+++ scripts/house/kit_bed.py
@@ -0,0 +1,27 @@
+# Instruction
+# -----------
+# 1. Using a combination of list sub-setting and
+#    variable assignment, create a new variable,
+#    eat_sleep_area, that contains the sum of
+#    the area of the kitchen and the area of the
+#    bedroom.
+# 2. Print the new variable eat_sleep_area.
+
+
+
+# Solution
+# --------
+# Create the areas list
+areas = ["hallway", 11.25,
+         "kitchen", 18.0,
+         "living room", 20.0,
+         "bedroom", 10.75,
+         "bathroom", 9.50]
+
+# Sum of kitchen and bedroom area:
+# eat_sleep_area
+
+eat_sleep_area = areas[3] + areas[7]
+
+# Print the variable eat_sleep_area
+print(eat_sleep_area)
