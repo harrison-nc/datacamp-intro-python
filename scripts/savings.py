6aae781faa87abb8807e87f2021fd0db8a6500d9
Harrison Chikwe <nhcdeveloper@gmail.com>
2023-01-29 20:41:48 +0100

    Use operator: exponentiation

---
+++ scripts/savings.py
@@ -0,0 +1,24 @@
+# Instruction
+# -----------
+# 1. Create a variable growth_multiplier, equal
+#    to 1.1 .
+# 2. Create a variable, result, equal to the
+#    amount of money you saved after 7 years
+# 3. Print out the value of result.
+
+
+
+# Solution
+# ---------
+# Create a variable savings.
+savings = 100
+
+# Create a variable growth_multiplier
+growth_multiplier = 1.1
+duration = 7
+
+# Calculate result
+result = savings * growth_multiplier ** duration
+
+# Print out result
+print(result)
