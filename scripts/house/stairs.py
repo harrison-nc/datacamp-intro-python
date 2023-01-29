ac1a41cdb7a1483d2ff0dd9caae580c473957453
Harrison Chikwe <nhcdeveloper@gmail.com>
2023-01-29 22:06:30 +0100

    Use data type: list slicing

---
+++ scripts/house/stairs.py
@@ -0,0 +1,29 @@
+# Instruction
+# -----------
+# 1. Use slicing to create a list, downstairs,
+#    that contains the first 6 elements of areas.
+# 2. Do a similar thing to create a new variable,
+#    upstairs that contains the last 4 elements
+#    of areas.
+# 3. Print both downstairs and upstairs
+
+
+# Solution
+# --------
+# Create the areas list
+# Create the areas list
+areas = ["hallway", 11.25,
+         "kitchen", 18.0,
+         "living room", 20.0,
+         "bedroom", 10.75,
+         "bathroom", 9.50]
+
+# Use slicing to create downstairs
+downstairs = areas[0:6]
+
+# Use slicing to create upstairs
+upstairs = areas[6:10]
+
+# Print out downstairs and upstairs
+print(downstairs)
+print(upstairs)
