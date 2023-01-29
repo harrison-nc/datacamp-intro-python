f2e091d38495b5a29e11dc436b351037eabd20f8
Harrison Chikwe <nhcdeveloper@gmail.com>
2023-01-29 20:51:50 +0100

    Use operator: string concatenation

---
+++ scripts/savings_desc.py
@@ -0,0 +1,35 @@
+# Instruction
+# -----------
+# 1. Calculate the product of savings and
+#    growth_multiplier. Store the result in
+#    year1.
+# 2. What do you think the resulting type will
+#    be? Find out by printing out the type of
+#    year1.
+# 3. Calculate the sum of desc and desc and
+#    store the result in a new variable
+#    double_desc.
+# 4. Print out double_desc. Did you expect
+#    this?
+
+
+# Solution
+# --------
+
+savings = 100
+growth_multiplier = 1.1
+desc = "compound interest"
+
+# Assign product of savings and 
+# growth_multiplier to year1
+
+year1 = savings * growth_multiplier
+
+# Print the type of year1
+print(type(year1))
+
+# Assign sum of desc and desc to double_desc
+double_desc = desc + desc
+
+# Print out double_desc
+print(double_desc)
